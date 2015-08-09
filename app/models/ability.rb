6016fa056f7e189c43778cbef2a4f0b73da4dfa8
Kimberly Davis <kwatsondavis@gmail.com>
2015-08-09 22:43:39 +0000

    Added ability to destroy comments for admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    # user ||= User.new # guest user (not logged in)
+    # can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
     # Define abilities for the passed in user here. For example:
     #
